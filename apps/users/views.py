01a6e5a69838b8276c7099f5ba6dae372301cc0b
wayne <wayne@magedu.com>
2018-11-21 15:28:01 +0800

    forms实现了高亮显示错误， 定义的username和pwd的格式

--- apps/users/views.py
+++ apps/users/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
+from django.views.generic.base import View
 
+from .forms import LoginForm
 from .models import UserProfile
 
 
@@ -19,16 +21,21 @@ class CustomBacken(ModelBackend):
             return None
 
 
-# Create your views here.
-def user_login(request):
-    if request.method == 'POST':
-        user_name = request.POST.get('username', '')
-        pass_word = request.POST.get('password', '')
-        user = authenticate(username=user_name, password=pass_word)
-        if user is not None:
-            login(request, user)
-            return render(request, 'index.html')
-        else:
-            return render(request, 'login.html', {"msg":"用户名或密码错误"})
-    elif request.method == 'GET':
+class LoginView(View):
+    def get(self, request):
         return render(request, 'login.html', {})
+    def post(self, request):
+        login_form = LoginForm(request.POST) #todo  还没有解决forms中errors==None的问题。
+        if login_form.is_valid():
+            user_name = request.POST.get('username', '')
+            pass_word = request.POST.get('password', '')
+            user = authenticate(username=user_name, password=pass_word)
+            if user is not None:
+                login(request, user)
+                return render(request, 'index.html')
+            else:
+                return render(request, 'login.html', {"msg": "用户名或密码错误"})
+        else:
+            return render(request, "login.html", {"msg": "用户名或密码错误", "login_form":login_form})
+
+
